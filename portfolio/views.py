c7949f9cd820b436f6c1aefcc56217016cf4c2a4
Parker Neff <pneff@unicon.net>
2020-12-15 08:17:02 -0700

    finished lesson 41

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Project
 
-# Create your views here.
+
+def index(request):
+    projects = Project.objects.all()
+    template = loader.get_template('portfolio/index.html')
+    return HttpResponse(template.render({'projects': projects}, request))
---
+++ readme.md
@@ -0,0 +1,1 @@
+Last lesson: 41
