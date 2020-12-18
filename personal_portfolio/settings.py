32e868077a48e865da379a3e9ecaa1ca393caf87
Parker Neff <parkerneff@gmail.com>
2020-12-18 20:30:26 +0000

    hello

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '9@1f%#j(0eg4)^v+z%wl$vw1e4zzyrh54%s5u*00wfj@7)ja@c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['parkerneff.pythonanywhere.com']
 
 
 # Application definition
@@ -122,5 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
